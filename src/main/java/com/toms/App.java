e93ff8d60cf75184ef04de160a98a90fde25bbda
toms666 <669568029@qq.com>
2020-04-30 12:44:56 +0800

    toms666 edit

--- src/main/java/com/toms/App.java
+++ src/main/java/com/toms/App.java
@@ -1,5 +1,7 @@
 package com.toms;
 
+import com.toms.service.UserService;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "这是用来练习上传的第一个项目" );
+        UserService su=new UserService();
+        System.out.println( su.get());
+
     }
 }
---
+++ src/main/java/com/toms/service/UserService.java
@@ -0,0 +1,7 @@
+package com.toms.service;
+
+public class UserService {
+    public String get(){
+        return "这是github";
+    }
+}
