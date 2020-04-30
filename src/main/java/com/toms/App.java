9e9f49e53f107a6ed639b7da0b8236f94de3516c
toms666 <669568029@qq.com>
2020-04-30 12:38:51 +0800

    toms666 edit

---
+++ src/main/java/com/toms/App.java
@@ -0,0 +1,13 @@
+package com.toms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "这是用来练习上传的第一个项目" );
+    }
+}
